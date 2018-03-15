b69a6da3b651e3b66d18ba59fc350f11e917a3d4
yinm <yinm.cminor9@gmail.com>
2018-03-15 09:29:29 +0900

    Check basic types

--- basic-types.go
+++ basic-types.go
@@ -6,15 +6,13 @@ import (
 )
 
 var (
-	ToBe   bool       = false
-	MaxInt uint64     = 1<<64 - 1
-	z      complex128 = cmplx.Sqrt(-5 + 12i)
+	ToBe bool = false
+	MaxInt uint64 = 1<<64-1
+	z complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	const f = "%T(%v)\n"
-	fmt.Printf(f, ToBe, ToBe)
-	fmt.Printf(f, MaxInt, MaxInt)
-	fmt.Printf(f, z, z)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
-

d5f4f143300c3d93ef58e8ef405fea8c45c77416
yinm <yinm.cminor9@gmail.com>
2018-03-19 08:40:04 +0900

    Check range continued

--- range-continued.go
+++ range-continued.go
@@ -5,10 +5,10 @@ import "fmt"
 func main() {
 	pow := make([]int, 10)
 	for i := range pow {
-		pow[i] = 1 << uint(i) // == 2**1
+		pow[i] = 1 << uint(i)
 	}
+
 	for _, value := range pow {
 		fmt.Printf("%d\n", value)
 	}
 }
-

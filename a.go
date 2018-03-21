90e8e192ee039dbb0bc0a56441136fcac6f6a3f4
yinm <yinm.cminor9@gmail.com>
2018-03-21 19:49:48 +0900

    Check slices of slice

--- a.go
+++ a.go
@@ -1,27 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	a := make([]int, 5)
-	printSlice("a", a)
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
 
-	b := make([]int, 0, 5)
-	printSlice("b", b)
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
 
-	c := b[:2]
-	printSlice("c", c)
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 
-	d := c[2:5]
-	printSlice("d", d)
-
-	e := make([]int, 0, 5)
-	printSlice("e", e)
-
-	f := e[2:5]
-	printSlice("f", f)
-}
-
-func printSlice(s string, x []int) {
-	fmt.Printf("%s len=%d cap=%d %v\n", s, len(x), cap(x), x)
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", board[i])
+	}
 }

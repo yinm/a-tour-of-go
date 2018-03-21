13da5275ba3a85bab3f79bf81c87915aa8f52916
yinm <yinm.cminor9@gmail.com>
2018-03-21 10:36:26 +0900

    Check function closure

--- function-closures.go
+++ function-closures.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func adder() func(int) int {
 	sum := 0
+
 	return func(x int) int {
 		sum += x
 		return sum
@@ -12,11 +13,11 @@ func adder() func(int) int {
 
 func main() {
 	pos, neg := adder(), adder()
+
 	for i := 0; i < 10; i++ {
 		fmt.Println(
 			pos(i),
-			neg(-2*i),
+			neg(-2 * i),
 		)
 	}
 }
-

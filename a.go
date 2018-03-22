1cfe76128734354b929b2dc06353ea61bf3cf537
yinm <yinm.cminor9@gmail.com>
2018-03-22 21:56:51 +0900

    Check methods pointers explained

--- a.go
+++ a.go
@@ -1,4 +1,3 @@
-// 2nd
 package main
 
 import (
@@ -10,19 +9,17 @@ type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v *Vertex) Scale(f float64) {
+func Scale(v *Vertex, f float64) {
 	v.X = v.X * f
 	v.Y = v.Y * f
-	fmt.Println(v.X, v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	v.Scale(10)
-	fmt.Println(v.X, v.Y)
-	fmt.Println(v.Abs())
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
 }

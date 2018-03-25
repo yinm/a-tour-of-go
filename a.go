962ee7cac717c581581602dae259e9f8fa120ebd
yinm <yinm.cminor9@gmail.com>
2018-03-25 20:43:54 +0900

    Check goroutine

--- a.go
+++ a.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
-	"image"
+	"time"
 )
 
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
 func main() {
-	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	fmt.Println(m.Bounds())
-	fmt.Println(m.At(0, 0). RGBA())
+	go say("world")
+	say("hello")
 }

02528862d931c1c3c00ef491a8d65fdaaeed7beb
yinm <yinm.cminor9@gmail.com>
2018-03-27 08:02:13 +0900

    Check range

--- a.go
+++ a.go
@@ -2,20 +2,10 @@ package main
 
 import "fmt"
 
+var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
 func main() {
-	var s []int
-	printSlice(s)
-
-	s = append(s, 0)
-	printSlice(s)
-
-	s = append(s, 1)
-	printSlice(s)
-
-	s = append(s, 2, 3, 4)
-	printSlice(s)
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }

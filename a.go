056dc6c591c2a449871a6ea4e6ce3631e55a4494
yinm <yinm.cminor9@gmail.com>
2018-03-26 13:15:04 +0900

    Check structs

--- a.go
+++ a.go
@@ -2,15 +2,11 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	X int
+	Y int
+}
+
 func main() {
-	i, j := 42, 2701
-
-	p := &i
-	fmt.Println(*p)
-	*p = 21
-	fmt.Println(i)
-
-	p = &j
-	*p = *p / 37
-	fmt.Println(j)
+	fmt.Println(Vertex{1, 2})
 }

aefe9f952f41c974126d9a49b93f933268fad618
yinm <yinm.cminor9@gmail.com>
2018-03-29 09:30:53 +0900

    Check exercise maps

--- a.go
+++ a.go
@@ -1,19 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	wordToCount := make(map[string]int)
+	for _, word := range words {
+		wordToCount[word]++
+	}
+	return wordToCount
+}
 
 func main() {
-	m := make(map[string]int)
-
-	m["Answer"] = 42
-	fmt.Println("The value:", m["Answer"])
-
-	m["Answer"] = 48
-	fmt.Println("The value:", m["Answer"])
-
-	delete(m, "Answer")
-	fmt.Println("The value:", m["Answer"])
-
-	v, ok := m["Answer"]
-	fmt.Println("The value:", v, "Present?", ok)
+	wc.Test(WordCount)
 }

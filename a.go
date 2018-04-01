887c2f8ec456ff197f9baecd055f3713b4a97c5e
yinm <yinm.cminor9@gmail.com>
2018-04-01 09:09:30 +0900

    Check interfaces are satisfied implicitly

--- a.go
+++ a.go
@@ -1,40 +1,20 @@
-// 2nd
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type Abser interface {
-	Abs() float64
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+func (t T) M() {
+	fmt.Println(t.S)
 }
 
 func main() {
-	var a, b Abser
-	f := MyFloat(-math.Sqrt2)
-	v := Vertex{3, 4}
-
-	a = f
-	b = &v
-
-	fmt.Println(a.Abs())
-	fmt.Println(b.Abs())
-}
-
-type MyFloat float64
-
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
-
-type Vertex struct {
-	X, Y float64
-}
-
-func (v *Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+	var i I = T{"hello"}
+	i.M()
 }

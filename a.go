78f799bd1872fd340253ba9e05a9569cea67a55b
yinm <yinm.cminor9@gmail.com>
2018-04-02 08:04:24 +0900

    Check interface values

--- a.go
+++ a.go
@@ -1,7 +1,9 @@
-// 3rd
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type I interface {
 	M()
@@ -11,11 +13,27 @@ type T struct {
 	S string
 }
 
-func (t T) M() {
+func (t *T) M() {
 	fmt.Println(t.S)
 }
 
+type F float64
+
+func (f F) M() {
+	fmt.Println(f)
+}
+
 func main() {
-	var i I = T{"hello"}
+	var i I
+	i = &T{"Hello"}
+	describe(i)
 	i.M()
+
+	i = F(math.Pi)
+	describe(i)
+	i.M()
+}
+
+func describe(i I) {
+	fmt.Printf("(%v, %T)\n", i, i)
 }

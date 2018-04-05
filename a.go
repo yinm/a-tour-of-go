2d3a33bf6874f03ad5f3ff2f1804eb0ddce48190
yinm <yinm.cminor9@gmail.com>
2018-04-05 09:15:21 +0900

    Check Errors

--- a.go
+++ a.go
@@ -1,30 +1,30 @@
-// 2nd
 package main
 
 import (
 	"fmt"
-	"time"
+	"math"
 )
 
-type MyError struct {
-	When time.Time
-	What string
+type ErrNegativeSqrt float64
+
+func (e ErrNegativeSqrt) Error() string {
+	return fmt.Sprintf("cannot Sqrt negative number: %f", e)
 }
 
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s", e.When, e.What)
-}
-
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
 	}
+
+	z := 1.0
+	for diff := 1.0; math.Abs(diff) > 1e-10; {
+		diff = ((z * z) - x) / (2 * z)
+		z = z - diff
+	}
+	return z, nil
 }
 
 func main() {
-	err := run()
-	if err != nil {
-		fmt.Println(err)
-	}
+	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(-2))
 }

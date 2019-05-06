3aa1a789ddd09718a31f693d2a347b7ed978903e
yinm <yinm.cminor9@gmail.com>
2019-05-06 20:28:18 +0900

    Check packages

--- sample.go
+++ sample.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand"
 )
 
 func main() {
-	var x, y int = 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y*y))
-	var z uint = uint(f)
-	fmt.Println(x, y, z)
+	fmt.Println("My favorite number is", rand.Intn(10))
 }
